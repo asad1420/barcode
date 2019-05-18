b02e2efab26df08aa17f4e0d4d205ea66699a679
asad1420 <asadnawaz94@outlook.com>
2019-05-18 17:58:54 +0200

    ...

--- barcode/ViewController.swift
+++ barcode/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var userInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,66 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func barcode(_ sender: Any) {
+        
+        imageDisplay.image = generateBarcodeFromString(string: userInput.text!)
+    }
 
+    @IBAction func qrCode(_ sender: Any) {
+        
+        imageDisplay.image = generateQRcodeFromString(string: userInput.text!)
+    }
+   
+    
+    func generateBarcodeFromString(string: String) -> UIImage? {
+    
+    let data = string.data(using: .ascii)
+    let filter = CIFilter(name: "CICode128BarcodeGenerator")
+        
+        filter?.setValue(data, forKey: "inputMessage")
+        
+        let transform = CGAffineTransform(scaleX: 10,y: 10)
+        
+        let output = filter?.outputImage?.transformed(by: transform)
+        
+        if (output != nil) {
+        
+        let result =  convert(cmage: output!)
+            return result
+        }
+        return nil;
+        
+        
+    }
+    
+    func generateQRcodeFromString(string: String) -> UIImage? {
+        
+        let data = string.data(using: .ascii)
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        filter?.setValue(data, forKey: "inputMessage")
+        
+        let transform = CGAffineTransform(scaleX: 10,y: 10)
+        
+        let output = filter?.outputImage?.transformed(by: transform)
+        
+        if (output != nil) {
+            
+            let result =  convert(cmage: output!)
+            return result
+        }
+        return nil;
+        
+        
+    }
+    
+    
+    func convert(cmage:CIImage) -> UIImage
+    {
+        let context:CIContext = CIContext.init(options: nil)
+        let cgImage:CGImage = context.createCGImage(cmage, from: cmage.extent)!
+        let image:UIImage = UIImage.init(cgImage: cgImage)
+        return image
+    }
 }
 
---
+++ barcode/qrScan.swift
@@ -0,0 +1,89 @@
+
+
+
+import UIKit
+import AVFoundation
+
+class qrCodeScan: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+   
+    @IBOutlet weak var back: UIButton!
+    var video = AVCaptureVideoPreviewLayer()
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let session = AVCaptureSession()
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do{
+        
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        
+        }
+        catch{
+            
+            print("ERROR!!")
+        }
+        let output = AVCaptureMetadataOutput()
+            session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+    //    output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+      
+         output.metadataObjectTypes = [AVMetadataObjectTypeEAN13Code]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        self.view.bringSubview(toFront: back )
+        
+        session.startRunning()
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+    
+    @IBAction func backButton(_ sender: Any) {
+        
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "mainC")
+        self.present(viewController, animated: true, completion: nil)
+        
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        if metadataObjects != nil && metadataObjects.count != 0 {
+        
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+            
+        //        if object.type == AVMetadataObjectTypeQRCode {
+                 if object.type == AVMetadataObjectTypeEAN13Code {
+                
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                
+                    present(alert, animated: true, completion: nil)
+              
+                }
+            
+            }
+        
+        
+        }
+    }
+
+}
